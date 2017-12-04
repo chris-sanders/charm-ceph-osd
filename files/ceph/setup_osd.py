9eeb960ad2d37acc035ea2aeb9b9624f70dbeb16
Chris Sanders <sanders.chris@gmail.com>
2017-12-04 15:23:56 -0600

    Add osd-shared, create osd on mounted devices
    Addes logic to create and activate partitions on devices that have
    mounted partitions. No changes are made to hooks, the utils library is
    updated to handle partitions where it previously bailed.
    * Build ceph-disk flags in function to re-use current logic in
    osdize_dev
    * Shared osd's implement all options/logic of the osdize_dev
     * Checks for in use
     * Handles the format osd option
     * Works with dmcrypt
     * Will not format/zap non-ceph partitions
    
    This includes two drive-by fixes for bugs which it didn't make since to
    break out. Both changes are in code which was altered in this
    patch and not closing them would require implementing the bug to avoid
    closing it.
    * Partitions are wiped before zapping
    * Zap is done via charmhelpers instead of using the ceph-disk option
    
    Closes-Bug: #1743278
    Closes-Bug: #1743280
    Closes-Bug: #1743282
    
    Change-Id: I2450a427520b6713abc457374421c94bb7f16078

---
+++ files/ceph/setup_osd.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+
+import sys
+import copy
+
+from ceph_disk import main as ceph_disk
+
+
+def setup_data(args, factory):
+    # Setup data partition
+    factory.data.set_variables()
+    factory.data.device = ceph_disk.Device.factory(args.data, args)
+
+    partition_num = factory.data.device.create_partition(uuid=args.osd_uuid,
+                                                         name='data',
+                                                         size=factory.data.get_space_size())
+    factory.data.partition = factory.data.device.get_partition(partition_num)
+
+
+def create_partitions(argv):
+    args = ceph_disk.parse_args(argv)
+    ceph_disk.setup_logging(args.verbose, args.log_stdout)
+    ceph_disk.setup_statedir(args.statedir)
+    ceph_disk.setup_sysconfdir(args.sysconfdir)
+    factory = ceph_disk.Prepare(args).factory(args)
+
+    if args.dmcrypt:
+        # Setup lockbox partition
+        lockbox = ceph_disk.Lockbox(args)
+        device_args = copy.copy(args)
+        device_args.dmcrypt = False
+        lockbox.device = ceph_disk.Device.factory(args.lockbox, device_args)
+        partition_num = lockbox.device.create_partition(uuid=args.lockbox_uuid,
+                                                        name='lockbox',
+                                                        size=10)
+        lockbox.partition = lockbox.device.get_partition(partition_num)
+        factory.lockbox = lockbox
+        factory.lockbox.populate()
+
+        # Bluestore
+    if args.bluestore:
+        setup_data(args, factory)
+        populate_list = []
+        # Setup block
+        if args.block == args.data:
+            args.data = 'dummy'
+            factory.block.prepare()
+            args.data = args.block
+        else:
+            factory.block.prepare()
+        populate_list.append(factory.block)
+
+        # Setup block.db
+        if getattr(args, 'block.db'):
+            if getattr(args, 'block.db') == args.data:
+                args.data = 'dummy'
+                factory.blockdb.prepare()
+                args.data = getattr(args, 'block.db')
+            else:
+                factory.blockdb.prepare()
+            populate_list.append(factory.blockdb)
+
+        # Setup block.wal
+        if getattr(args, 'block.wal'):
+            if getattr(args, 'block.wal') == args.data:
+                args.data = 'dummy'
+                factory.blockwal.prepare()
+                args.data = getattr(args, 'block.wal')
+            else:
+                factory.blockwal.prepare()
+            populate_list.append(factory.blockwal)
+
+        # Populate
+        factory.data.populate_data_path_device(*populate_list)
+    # Filestore
+    else:
+        # Setup journal
+        if args.journal == args.data:
+            args.data = 'dummy'
+            factory.journal.prepare()
+            args.data = args.journal
+        else:
+            factory.journal.prepare()
+        setup_data(args, factory)
+        # Populate data
+        factory.data.populate_data_path_device(factory.journal)
+
+
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    create_partitions(argv)
+
